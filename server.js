3733abad4936e58c683d4bca323e865e78dab8b7
gabconni911 <gabriemikol@gmail.com>
2017-03-18 02:39:51 +0800

    deploy to heroku

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ app.get('/', function(req, res){
 app.get('/app', function(req, res){
   res.sendFile(path.join(__dirname, 'app.html'));
 });
+app.get('/main', function(req, res){
+  res.sendFile(path.join(__dirname, 'main.html'));
+});
+app.get('/app', function(req, res){
+  res.sendFile(path.join(__dirname, 'app.html'));
+});
 app.get('*', function(req, res){
   res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
 });
